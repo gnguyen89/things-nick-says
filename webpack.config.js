f99322b103e526324134a228f79a9a251059792f
Giang Nguyen <luckytintinnz@yahoo.com>
2016-11-09 02:05:47 +1300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+module.exports = {
+  entry: [
+    './src/app.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      {test: /\.(png|jpg|ico)$/, loader: 'file-loader?name=img/[name].[ext]'},
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
