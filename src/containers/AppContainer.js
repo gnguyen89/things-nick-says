477f29e43cd8b8b2a7d3b3a4d209617921462fd7
Nick Ng <nick-ng@users.noreply.github.com>
2016-12-09 18:00:07 +1300

    Merge changes in dev to master. (#12)
    * initial design
    
    * fix styling issues
    
    * add procfile and server script
    
    * add heroku post build
    
    * Trying to fix thing.
    
    * fixing webpack config
    
    * blah
    
    * fix package file
    
    * add quoteService and mock data
    
    * add more quotes
    
    * fix style for mobile device
    
    * Fix/correct some quotes (#2)
    
    * Corrected the "street crossing" quote.
    
    * Changed magic string to constants file.
    
    * Update quotes.json (#3)
    
    * added favicon support (#4)
    
    * Quote cycle (#5)
    
    * added favicon support
    
    * removed unused routes and components
    
    * rotate quotes after 5 seconds
    
    * Added "Fact" quote type.
    
    * Made facts italic.
    
    * read quotes from firebase (#10)
    
    * Added "from a hat without replacement" randomization to quotes.

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,26 +1,47 @@
 import React, { PropTypes, Component } from 'react';
 import css from '../styles/app.css';
 import quoteService from '../services/quoteService';
+import watchFirebase from '../services/firebaseClient';
 
 export default class AppContainer extends Component {
   constructor() {
     super();
 
     this.updateTimer = this.updateTimer.bind(this);
-    setInterval(this.updateTimer, 9999);
-    const quote = quoteService.getRandomQuote();
+    this.updateQuotes = this.updateQuotes.bind(this);
+
+    watchFirebase('/quotes', this.updateQuotes);
     this.state = {
-      quote,
+      quote: '',
+      intervalRef: null,
     };
   }
 
   updateTimer() {
-    this.setState({ quote: quoteService.getRandomQuote() });
+    const { quotes } = this.state;
+    if (quotes && quotes.length) {
+      this.setState(quoteService.getRandomQuote(quotes, this.state.quoteHat));
+    }
+  }
+  updateQuotes(quotes) {
+    const { intervalRef, quote: quoteInState } = this.state;
+
+    this.setState(Object.assign(
+      {},
+      { quotes },
+      quoteInState ? quoteService.getRandomQuote(quotes, this.state.quoteHat) : null,
+    ));
+
+    if (!intervalRef) {
+      const newRef = setInterval(this.updateTimer, 9999);
+      this.setState({ intervalRef: newRef });
+    }
   }
 
   render() {
     const { children } = this.props;
     const { quote } = this.state;
+    if (!quote) return null;
     return <div className={css.appContainer}>
       {children && React.cloneElement(this.props.children, {
         quote,
